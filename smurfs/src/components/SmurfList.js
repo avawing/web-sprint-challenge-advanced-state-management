980ffc401bb5cf049c4e738001f911edf6544e57
Aveline Wingfield <avelinewingfield@gmail.com>
2020-08-14 12:25:36 -0400

    Use Effect implemented

---
+++ smurfs/src/components/SmurfList.js
@@ -0,0 +1,26 @@
+import React, {useEffect} from 'react'
+import {connect} from 'react-redux'
+import {fetchSmurfs} from '../actions'
+
+const mapStateToProps = (state) => {
+    return {
+        smurfs: state.smurfs,
+        isLoading: state.isLoading,
+        errors: state.errors
+    };
+  };
+
+function SmurfList(props){
+    useEffect(()=>{
+        props.fetchSmurfs()
+    }, [])
+
+    return(
+        <div>
+        <h1>The Smurf Village</h1>
+        {console.log(props.smurfs)}
+    {props.smurfs.length > 0 ? props.smurfs.map(smurf => {return <h2>Name: {smurf.name}</h2> }) : null}</div>
+    )
+}
+
+export default connect(mapStateToProps, {fetchSmurfs})(SmurfList)
